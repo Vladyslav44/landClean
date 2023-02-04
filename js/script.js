27d0b9639f20f3b4845695a2023a3d6c833136d8
vladyslavsaienko <vladyslav.saienko@jcash.co>
2023-02-04 15:09:54 +0200

    zalupa

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ button12.addEventListener("click", function() {
 
 const modalButton = document.getElementById("modal-button");
 const modal = document.getElementById("modal");
-const closeButton = document.querySelector(".close-button");
+const closeButton = document.getElementById("close-button");
 
 const showModal = () => {
 	modal.style.display = 'block';
@@ -91,22 +91,49 @@ document.addEventListener("DOMContentLoaded", function() {
 	const links = document.querySelectorAll(".link");
 
 	// Add click event listeners to each link
+	const modalTexts = [
+		"Lorem Ipsum, you need to be sure there " +
+		"isn't anything embarrassing hidden in the middle of text " +
+		"Lorem Ipsum, you need to be sure there isn't " +
+		"anything embarrassing hidden in the middle of text",
+		"Lorem Ipsum, you need to be sure there " +
+		"isn't anything embarrassing hidden in the middle of text " +
+		"Lorem Ipsum, you need to be sure there isn't " +
+		"anything embarrassing hidden in the middle of text",
+		"Lorem Ipsum, you need to be sure there " +
+		"isn't anything embarrassing hidden in the middle of text " +
+		"Lorem Ipsum, you need to be sure there isn't " +
+		"anything embarrassing hidden in the middle of text"
+	];
+
 	links.forEach((link, index) => {
 		link.addEventListener("click", function() {
-			modalText.innerHTML = `Link ${index + 1} was clicked.`;
+			const linkText = modalTexts[index];
+			modalText.innerHTML = linkText;
 			modal.style.display = "block";
+			setTimeout(() => {
+				modal.style.opacity = "1";
+				modal.style.transform = "scale(1)";
+			}, 100);
 		});
 	});
 
 	// Add a click event listener to the close button
 	closeBtn.addEventListener("click", function() {
-		modal.style.display = "none";
+		modal.style.opacity = "0";
+		modal.style.transform = "scale(0.5)";
+		setTimeout(() => {
+			modal.style.display = "none";
+		}, 400);
 	});
 
 	// Add a click event listener to the window to close the modal when clicked outside
-	window.addEventListener("click", function(event) {
-		if (event.target === modal) {
-			modal.style.display = "none";
+	modal.addEventListener("click", function(e) {
+		if (e.target === modal) {
+			modal.style.opacity = "0";
+			setTimeout(() => {
+				modal.style.display = "none";
+			}, 500);
 		}
 	});
 });
